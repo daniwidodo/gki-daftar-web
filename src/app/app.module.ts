a2eed7b1f8da78001a269fc8462527ffcf2c98ae
bangtur_studio <dani.widodo87@gmail.com>
2021-10-30 10:08:54 +0800

    build v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HttpClientModule } from '@angular/common/http';
 import { NgxQRCodeModule } from '@techiediaries/ngx-qrcode';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 ///////
 @NgModule({
@@ -21,7 +23,13 @@ import { NgxQRCodeModule } from '@techiediaries/ngx-qrcode';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    NgxQRCodeModule
+    NgxQRCodeModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     {
